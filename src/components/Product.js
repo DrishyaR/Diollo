0d446e71aa1fb21500387794b03d22d3807f3124
DrishyaR <drishyarameshpk@gmail.com>
2019-09-21 23:30:37 +0530

    Updated with CSS features

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import {ProductConsumer} from '../context';
+import Cart from '../shop.png';
 
 export default class Product extends Component {
   render() {
@@ -13,6 +14,27 @@ export default class Product extends Component {
             <Link to="/details">
               <img src={img} alt="Not available" className="card-img-top" />
             </Link>
+            <button className="cart-btn" disabled={inCart?true:false} onClick={()=>{console.log("added")}}>
+            {
+              inCart ? (
+                <p className="text-capitalize mb=0" disabled>
+                {" "}
+                in cart
+                </p>
+              ) : (
+                <img src={Cart} alt="store" className="navbar-brand" />
+              )
+            }
+            </button>
+          </div>
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb=0">
+              {title}
+            </p>
+            <h5 className="text-red font-italic mb=0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
           </div>
         </div>
       </ProductWrapper>
@@ -21,4 +43,22 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
-`
+  .card{
+    border-color: transparent;
+    transition: all 1s linear;    
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;    
+    &:hover {
+      .card {
+        border: 0.04rem solid rgba(0,0,0,0.8);
+        box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+      }
+      .card-footer {
+        background: rgba(247, 247, 247);
+      }
+    }
+  }
+`;
