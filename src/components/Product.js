fae727accc47d0e1a078d0e1f93e45ca5fd574f1
DrishyaR <drishyarameshpk@gmail.com>
2019-10-05 23:08:22 +0530

    Added a CSS features

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,7 +10,7 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div className="img-card p-5" onClick={()=> console.log('clicked')}>
+          <div className="img-container p-5" onClick={()=> console.log('clicked')}>
             <Link to="/details">
               <img src={img} alt="Not available" className="card-img-top" />
             </Link>
@@ -43,22 +43,41 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
-  .card{
+  .card {
     border-color: transparent;
-    transition: all 1s linear;    
+    transition: all 0.5s linear;    
   }
   .card-footer {
     background: transparent;
     border-top: transparent;
     transition: all 1s linear;    
-    &:hover {
-      .card {
-        border: 0.04rem solid rgba(0,0,0,0.8);
-        box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
-      }
-      .card-footer {
-        background: rgba(247, 247, 247);
-      }
+  }
+  &:hover {
+    .card {
+        border: 0.3rem solid rgba(0, 0, 0, 0.2);
+        box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
     }
+    .card-footer {
+        background: rgba(247, 247, 247);
+    }
+  } 
+  .img-container {
+        position: relative;
+        overflow: hidden;
+  }
+  .card-img-top {
+      transition: all 0.5s linear;    
+  }
+  .img-container:hover .card-img-top {
+      transform: scale(1.2);
+  }
+  .cart-btn {
+      position: relative;
+      bottom: 0;
+      right: 0;
+      padding: 0.2rem 0.4rem;
+      background: var(--yellow);
+      color: var(--white);
+      border-radius: 0.05rem;
   }
 `;
